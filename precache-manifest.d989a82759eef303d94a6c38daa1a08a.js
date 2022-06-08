3c6b810e58e9e6cedda700a6cee796b8a8dba9b1
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2022-06-08 21:01:33 +0200

    Updates

---
+++ precache-manifest.d989a82759eef303d94a6c38daa1a08a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b35bc9e7931151cb62b9c5fb2bc026d",
+    "url": "/cumbre2022/index.html"
+  },
+  {
+    "revision": "4b7efaf2680e16683f66",
+    "url": "/cumbre2022/static/css/2.ed68ed35.chunk.css"
+  },
+  {
+    "revision": "3243c4e4b7f6b7dc36da",
+    "url": "/cumbre2022/static/css/main.217fe80c.chunk.css"
+  },
+  {
+    "revision": "4b7efaf2680e16683f66",
+    "url": "/cumbre2022/static/js/2.e8b42e28.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/cumbre2022/static/js/2.e8b42e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3243c4e4b7f6b7dc36da",
+    "url": "/cumbre2022/static/js/main.d36be7ec.chunk.js"
+  },
+  {
+    "revision": "862e0a6a942af00d86ea",
+    "url": "/cumbre2022/static/js/runtime-main.74f344bd.js"
+  },
+  {
+    "revision": "cf54b08f6a720b598d2de35d8a90f70f",
+    "url": "/cumbre2022/static/media/Englishcv.cf54b08f.pdf"
+  },
+  {
+    "revision": "6efbfe2d5c1f0d5f35602d7de88dad20",
+    "url": "/cumbre2022/static/media/Fotoperfil2.6efbfe2d.jpg"
+  },
+  {
+    "revision": "b44009281240cdebe6cfe44cd54e8eef",
+    "url": "/cumbre2022/static/media/Spanishcv.b4400928.pdf"
+  },
+  {
+    "revision": "785d67bc5ca65a149c6c9f55669f2377",
+    "url": "/cumbre2022/static/media/cumbre.785d67bc.jpg"
+  },
+  {
+    "revision": "82f5282d37955685482ae229aef0a87d",
+    "url": "/cumbre2022/static/media/cursoReact1.82f5282d.png"
+  },
+  {
+    "revision": "ec4524cda0d21537e5d6fd10ef680ec7",
+    "url": "/cumbre2022/static/media/cursoReact2.ec4524cd.png"
+  }
+]);
